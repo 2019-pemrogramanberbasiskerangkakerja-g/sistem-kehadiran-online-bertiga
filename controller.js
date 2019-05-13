c6b9c3507593297e6d9d75da3d0d5d2a3e032cbb
Faizal Khilmi Muzakki <faizal.muzakki@gmail.com>
2019-05-13 13:35:06 +0700

    hm

--- controller.js
+++ controller.js
@@ -10,20 +10,20 @@ exports.absen = function(req, res) {
     con.connect(function(err) {
         var query = "SELECT * FROM meetings WHERE kode_matkul = ? AND start_date <= NOW() AND end_date >= NOW()";
         con.query(query, [nrp], function (err, result, fields) {
-            if(result.length == 0) response.err('Kelas tidak ditemukan atau belum dimulai');
+            if(result.length == 0) response.err('Kelas tidak ditemukan atau belum dimulai', res);
             else{
                 var query = "SELECT * FROM participants WHERE kode_matkul = ? AND nrp = ?";
                 con.query(query, [kodeMatkul, nrp], function (err, result, fields) {
-                    if(result.length == 0) response.err('Maaf, anda bukan peserta kelas');
+                    if(result.length == 0) response.err('Maaf, anda bukan peserta kelas', res);
                     else{
-                        response.ok(result);
+                        response.ok(result, res);
                     }
                     console.log(result);
                 });
             }
         });
     });
-};
+}
 
 exports.tambahMahasiswa = function(req, res) {
     //console.log(req)
@@ -33,16 +33,16 @@ exports.tambahMahasiswa = function(req, res) {
 
     var query = "SELECT * FROM mahasiswas WHERE nrp = ?";
     con.query(query, [nrp], function (err, result, fields) {
-        if(result.length > 0) response.err('Mahasiswa sudah ada bro');
+        if(result.length > 0) response.err('Mahasiswa sudah ada bro', res);
         else{
             var query = "INSERT INTO mahasiswas(nrp, name, password) VALUES(?, ?, ?)";
             con.query(query, [nrp, name, password], function (err, result, fields) {
-                if(err) response.err('Maaf, ada error bro');
-                response.ok(result);
+                if(err) response.err('Maaf, ada error bro', res);
+                response.ok(result, res);
             });
         }
     });
-};
+}
 
 exports.tambahPeserta = function(req, res) {
     var nrp = req.params.nrp;
@@ -50,58 +50,65 @@ exports.tambahPeserta = function(req, res) {
 
     var query = "SELECT * FROM participants WHERE nrp = ? AND kode_matkul = ?";
     con.query(query, [nrp, kodeMatkul], function (err, result, fields) {
-        if(result.length > 0) response.err('Mahasiswa sudah jadi peserta bro');
+        if(result.length > 0) response.err('Mahasiswa sudah jadi peserta bro', res);
         else{
             var query = "SELECT * FROM mahasiswas WHERE nrp = ?";
             con.query(query, [nrp], function (err, result, fields) {
-                if(result.length == 0) response.err('Mahasiswanya jangan ngawur bro');
+                if(result.length == 0) response.err('Mahasiswanya jangan ngawur bro', res);
                 else{
-                    var query = "INSERT INTO participants(nrp, kode_matkul) VALUES(?, ?)";
-                    con.query(query, [nrp, kodeMatkul], function (err, result, fields) {
-                        if(err) response.err('Maaf, ada error bro');
-                        response.ok(result);
+                    var query = "SELECT * FROM matkuls WHERE kode = ?";
+                    con.query(query, [kodeMatkul], function (err, result, fields) {
+                        if(result.length == 0) response.err('Matkulnya jangan ngawur bro', res);
+                        else{
+                            var query = "INSERT INTO participants(nrp, kode_matkul) VALUES(?, ?)";
+                            con.query(query, [nrp, kodeMatkul], function (err, result, fields) {
+                                if(err) response.err('Maaf, ada error bro', res);
+                                response.ok(result, res);
+                            });
+                        }
                     });
                 }
             });
         }
     });
-};
+}
 
 exports.tambahMatkul = function(req, res) {
-    var kodeMatkul = parseInt(req.body.kodeMatkul);
+    var kodeMatkul = req.body.kodeMatkul;
     var name = req.body.name;
     var kelas = req.body.kelas;
     var semester = req.body.semester;
 
-    var query = "SELECT * FROM matkuls WHERE kelas="+kelas+"AND kode_matkul="+kodeMatkul;
-    con.query(query, function (err, result, fields) {
-        if(result.length > 0) response.err('Matkul sudah ada ternyata bro');
+    var query = "SELECT * FROM matkuls WHERE kelas = ? AND kode = ?";
+    con.query(query, [kelas, kodeMatkul], function (err, result, fields) {
+        if(result.length > 0) response.err('Matkul sudah ada ternyata bro', res);
         else{
-            var query = "INSERT INTO matkuls(kode_matkul, name, kelas, semester) VALUES(?, ?, ?, ?)";
+            var query = "INSERT INTO matkuls(kode, name, kelas, semester) VALUES(?, ?, ?, ?)";
             con.query(query, [kodeMatkul, name, kelas, semester], function (err, result, fields) {
-                if(err) response.err('Maaf, ada error bro');
-                response.ok(result);
+                if(err) response.err('Maaf, ada error bro', res);
+                response.ok(result, res);
             });
         }
     });
-};
+}
 
 exports.tambahJadwal = function(req, res) {
-    var kodeMatkul = parseInt(req.body.kodeMatkul);
-    var day = req.body.day;
+    var kodeMatkul = req.body.kodeMatkul;
+    var day = req.body.pertemuan;
     var ruang = req.body.ruang;
     var masuk = req.body.masuk;
     var selesai = req.body.selesai;
 
-    var query = "SELECT * FROM meetings WHERE ruang = ? AND masuk >= ? AND selesai <= ?";
+    var query = "SELECT * FROM meetings WHERE ruang = ? AND start_date >= ? AND end_date <= ?";
     con.query(query, [ruang, masuk, selesai], function (err, result, fields) {
-        if(result.length > 0) response.err('Kelas dah dipake bro');
+        console.log(result);
+        if(result.length > 0) response.err('Kelas dah dipake bro', res);
         else{
-            var query = "INSERT INTO meetings(kode_matkul, day, ruang, masuk, selesai) VALUES(?, ?, ?, ?, ?)";
+            var query = "INSERT INTO meetings(kode_matkul, day, ruang, start_date, end_date) VALUES(?, ?, ?, ?, ?)";
             con.query(query, [kodeMatkul, day, ruang, masuk, selesai], function (err, result, fields) {
-                if(err) response.err('Maaf, ada error bro');
-                response.ok(result);
+                if(err) response.err('Maaf, ada error bro', res);
+                response.ok(result, res);
             });
         }
     });
-};
+}

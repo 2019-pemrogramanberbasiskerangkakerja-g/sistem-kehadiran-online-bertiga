d329a367a8a5f29933d4ce3484e31b2412d65316
Faizal Khilmi Muzakki <faizal.muzakki@gmail.com>
2019-05-19 20:31:42 +0700

    init view

---
+++ controller-view.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var response = require('./res');
+var con = require('./conn');
+
+exports.tambahMahasiswa = function(req, res) {
+
+}
+
+exports.absen = function(req, res) {
+
+}
+
+exports.tambahPeserta = function(req, res) {
+
+}
+
+exports.tambahMatkul = function(req, res) {
+
+}
+
+exports.tambahJadwal = function(req, res) {
+
+}
+
+exports.tambahDosen = function(req,res) {
+
+}
+
+//rekap kuliah per semester
+exports.rekapPerSemester = function(req, res) {
+
+};
+
+//rekap kuliah per pertemuan
+exports.rekapPerPertemuan = function(req, res) {
+
+};
+
+//rekap mhs per kuliah
+exports.rekapMhsPerKuliah = function(req, res) {
+
+};
+
+//rekap mhs per semester
+exports.rekapMhsPerSemester = function(req, res) {
+    
+};
--- routes.js
+++ routes.js
@@ -2,22 +2,23 @@
 
 module.exports = function(app) {
     var FbpApi = require('./controller');
+    var FbpView = require('./controller-view');
 
     app.route('/absen/:ruang/:nrp')
         .post(FbpApi.absen);
 
     app.route('/rekap/:kodeMatkul')
         .get(FbpApi.rekapPerSemester);
-    
+
     app.route('/rekap/:kodeMatkul/:day')
         .get(FbpApi.rekapPerPertemuan);
-    
+
     app.route('/rekapmahasiswa/:nrp/:kodeMatkul')
         .get(FbpApi.rekapMhsPerKuliah);
-    
+
     app.route('/rekapmahasiswasem/:nrp/:semester')
         .get(FbpApi.rekapMhsPerSemester);
-    
+
     app.route('/tambahmahasiswa')
         .post(FbpApi.tambahMahasiswa);
 
@@ -26,10 +27,14 @@ module.exports = function(app) {
 
     app.route('/tambahmatkul')
         .post(FbpApi.tambahMatkul);
-    
+
     app.route('/tambahjadwal')
         .post(FbpApi.tambahJadwal);
 
     app.route('/tambahdosen')
         .post(FbpApi.tambahDosen);
+
+    // view
+    app.route('/tambahpeserta')
+        .get(FbpView.tambahMahasiswa)
 };
